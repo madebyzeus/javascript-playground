763c5d10c3714c0abf1ee1a0bf7ca493191e680d
Jesus Estrada <hellozues@gmail.com>
2020-05-14 14:28:50 -0700

    Added some more content to file

--- javascript-sandbox.js
+++ javascript-sandbox.js
@@ -5,7 +5,42 @@ let name = "Jesus";
 let quote = "Free pizza is never free."
 let currentLocation = "Ventura, CA"
 
-// Numbers
+// Numbers: Whole Numbers and Floats
 let myFavoriteNumber = 22
 let myLuckyNumber = 6
 let myAge = 39
+
+// Booleans: all written in lowercase
+true
+false 
+buttonHasBeenClicked = false;
+
+let myLocation = "Ventura, CA"
+let myOtherLocation = "Goleta, CA"
+
+myLocation === myOtherLocation // This is a comparison operator in JS
+
+// Objects, Arrays and More
+
+//  Example Objects
+let bird = {
+    genus: "corvus",
+    species: "corvax",
+    commonName: "raven",
+    callType: "squawky",
+    quote: "Nevermore",
+    maxOffspring: 5,
+    noisy: true,
+    deadly: false
+};
+
+let bear = {
+    genus: "ursus",
+    species: "arctos",
+    commonName: "brown bear",
+    callType: "roar",
+    quote: "",
+    maxOffspring: 3,
+    noisy: true,
+    deadly: true
+};
