8b4116e5b7e6da6f4e1d7ee90b1ea4b4afcee492
Jesus Estrada <hellozues@gmail.com>
2020-05-14 14:06:03 -0700

    Added some comments and data

---
+++ javascript-sandbox.js
@@ -0,0 +1,11 @@
+// Primitive Data Types: Strings, Numbers, Booleans, Undefined
+
+// Strings
+let name = "Jesus";
+let quote = "Free pizza is never free."
+let currentLocation = "Ventura, CA"
+
+// Numbers
+let myFavoriteNumber = 22
+let myLuckyNumber = 6
+let myAge = 39
